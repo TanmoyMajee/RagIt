793b2d449d16f9a9c1c77d32fe3a85dcf4f0224e
TanmoyMjaee <tanmoymajee239@gmail.com>
2025-07-13 23:19:52 +0530

    add fectch chat funcanality

--- client/src/pages/chatPage.tsx
+++ client/src/pages/chatPage.tsx
@@ -1,9 +1,36 @@
 import SessionBar from "../components/chat/SidebarSessions"
 import CharArea from "../components/chat/ChatArea"
 import Upload from "../components/chat/UploadPanel"
+import { useAuth } from "../context/AuthContext";
+import { useChatAuth } from "../context/ChatContext";
+import React, { useEffect} from "react"
+import { useParams } from 'react-router-dom';
 
-import React from "react"
+
 export const ChatPage: React.FC = ()=> {
+  const { sessionId } = useParams();
+  const { user,token } = useAuth();
+  const {fetchSession , fetchChatHistory , setSelectedSession} = useChatAuth();
+
+  // fecth all the session tile on first render or when user chage or when a new chat creatd
+  useEffect(()=>{
+    fetchSession();
+  },[user,token])
+
+  useEffect(() => {
+    if (sessionId) {
+      setSelectedSession(parseInt(sessionId))
+      const fetchData = async () => {
+        await fetchChatHistory(parseInt(sessionId));
+      };
+      fetchData();
+    }
+    // else: do nothing or show a default UI
+  }, [sessionId]);
+
+
+
+
   return (
     <div className="flex min-h-screen text-black">
      <SessionBar/>
--- client/src/pages/landingPage.tsx
+++ client/src/pages/landingPage.tsx
@@ -1,10 +1,16 @@
 
+import {Link} from 'react-router-dom'
 
 export const LandingPage = ()=>{
   return(
-    <div className="bg-gray-800">
-      <div className="bg-blue-500 text-white p-4">Hello</div>
+    <div className=" min-h-screen bg-gray-700 text-gray-300">
+      {/* <div className="bg-blue-500 text-white p-4">Hello</div> */}
          Here is the Landing Page
+         <div className='mt-10 ml-10'>
+            <Link to={'/chat'} className="bg-blue-500 ">
+            Launch the App
+            </Link>
+          </div>
     </div>
   )
 }
