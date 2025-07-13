38f2d60ef84b64ed17bcef266e00e056dca08807
TanmoyMjaee <tanmoymajee239@gmail.com>
2025-07-13 23:17:41 +0530

    wrap the chatProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,13 +5,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx';
+import { ChatProvider } from './context/ChatContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
+      <ChatProvider>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+      </ChatProvider>
     </AuthProvider>
   </StrictMode>,
 )
